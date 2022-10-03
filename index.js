5b3da6da7bc8f4bdcc0cbd6d4d6cf64789d9087e
projectsdatadna <107699067+projectsdatadna@users.noreply.github.com>
2022-10-03 15:43:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
-app.use(cors());
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+const express = require('express');
+const mysql = require ('mysql');
+const cors = require('cors')
 
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
+const db = mysql.createConnection({
+    "host": "localhost",
+    "user":"root",
+    "password": "",
+    "database":"nodedb"
 });
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+db.connect(function (error) {
 
+})
 
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+console.log('listening on port 8000');
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-    console.log(data.room, data)
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
+const wsServer = new webSocketServer({
+    httpServer: server
 });
 
-server.listen(5000, () => {
-  console.log("SERVER RUNNING 5000");
+const clients = {};
+
+const getUniqueID = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4() + s4() +  '=' + s4();
+};
+
+
+wsServer.on('request', function(request){
+    var userID = getUniqueID();
+    console.log((new Date()) + 'Recieved a new connection from origin' + request.origin + '.');
+
+    const connection = request.accept(null, request.origin);
+    clients[userID] = connection;
+    console.log('connected:' + userID + 'in' + Object.getOwnPropertyNames(clients));
+    
+    
+    connection.on('message', function(message){
+        if(message.type === 'utf8') {
+            console.log("typing");
+            console.log('Recieved message:', message.utf8Data);
+
+            for(key in clients) {
+                clients[key].sendUTF(message.utf8Data);
+                console.log('sent message to:', clients[key]);
+            }
+        }
+        db.query("INSERT INTO user_msg (firstname, message) VALUES ("+message.firstname+" ,"+message.message+")", function(error, result) {})
+    })
 });
+
+
